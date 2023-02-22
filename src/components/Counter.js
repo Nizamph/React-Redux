7872ae656b2e75e961aecee237dcf45a81935916
Nizamph <116118003+Nizamph@users.noreply.github.com>
2023-02-22 23:25:15 +0530

    increment and decrement using redux

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,41 @@
 import classes from './Counter.module.css';
-
+import { useSelector, useDispatch } from 'react-redux'
+import { useState } from 'react';
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+ const dispatch = useDispatch();
+ const counter =  useSelector(state => state.counter);
+ const [isShow, setIsShow] = useState(false)
+const incrementHandler = () => {
+  dispatch({ type: "increment" })
+};
 
-  return (
+const decrementHandler = () => {
+    dispatch({ type: "decrement" })
+}
+  const toggleCounterHandler = () => {
+    setIsShow((prevState) => {
+      return !prevState
+    })
+  };
+
+  const incrementBy5Handler = () => {
+    dispatch({ type:"incrementBy5" })
+  }
+
+  const decrementBy5Handler = () => {
+    dispatch({ type:"decrementBy5" })
+  }
+   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+      {!isShow && <button onClick={incrementHandler}>Increment</button>}
+       {!isShow && <button onClick={decrementHandler}>Decrement</button>}
+       {!isShow && <button onClick={incrementBy5Handler}>IncrementBy5</button>}
+       {!isShow && <button onClick={decrementBy5Handler}>DecrementBy5</button>}
+       
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
